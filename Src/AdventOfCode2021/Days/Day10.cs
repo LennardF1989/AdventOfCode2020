b791786f6a72ee95696726003a2ea9f6a1623fef
Lennard Fonteijn <lennard@lennardf1989.com>
2021-12-10 06:42:21 +0100

    Added Day 10 of AoC2021

--- Src/AdventOfCode2021/Days/Day10.cs
+++ Src/AdventOfCode2021/Days/Day10.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AdventOfCode.Shared;
 
 namespace AdventOfCode2021.Days
@@ -8,21 +11,91 @@ namespace AdventOfCode2021.Days
         public static void StartA()
         {
             var lines = File
-                .ReadAllLines("Content\\Day10.txt");
+                //.ReadAllLines("Content\\Day10_Test.txt")
+                .ReadAllLines("Content\\Day10.txt")
+                ;
 
-            int answer = 0;
+            int points = lines.Sum(x =>
+            {
+                var stack = new Stack<char>();
 
-            Logger.Info($"Day 10A: {answer}");
+                return CheckLine(x, stack);
+            });
+
+            Logger.Info($"Day 10A: {points}");
         }
 
         public static void StartB()
         {
             var lines = File
-                .ReadAllLines("Content\\Day10.txt");
+                //.ReadAllLines("Content\\Day10_Test.txt")
+                .ReadAllLines("Content\\Day10.txt")
+                ;
 
-            int answer = 0;
+            List<long> scores = new List<long>();
+
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+
+                int points = CheckLine(line, stack);
+
+                if (points != 0 || stack.Count <= 0)
+                {
+                    continue;
+                }
+
+                long localScore = 0;
+
+                while (stack.Count > 0)
+                {
+                    var c = stack.Pop();
+
+                    localScore = c switch
+                    {
+                        '(' => (localScore * 5) + 1,
+                        '[' => (localScore * 5) + 2,
+                        '{' => (localScore * 5) + 3,
+                        '<' => (localScore * 5) + 4,
+                        _ => localScore
+                    };
+                }
+
+                scores.Add(localScore);
+            }
+
+            scores.Sort();
+
+            long answer = scores[(int)Math.Floor(scores.Count / 2f)];
 
             Logger.Info($"Day 10B: {answer}");
+        }
+
+        private static int CheckLine(string line, Stack<char> stack)
+        {
+            foreach (var c in line)
+            {
+                switch (c)
+                {
+                    case '(' or '[' or '{' or '<':
+                        stack.Push(c);
+                        break;
+
+                    case ')' when stack.Pop() != '(':
+                        return 3;
+
+                    case ']' when stack.Pop() != '[':
+                        return 57;
+
+                    case '}' when stack.Pop() != '{':
+                        return 1197;
+
+                    case '>' when stack.Pop() != '<':
+                        return 25137;
+                }
+            }
+
+            return 0;
         }
     }
 }
