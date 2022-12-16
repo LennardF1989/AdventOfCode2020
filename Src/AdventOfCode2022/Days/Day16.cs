8dd0d0f8e724e9ba2bef0e9d75c0d30bab632dfd
Lennard Fonteijn <lennard@lennardf1989.com>
2022-12-16 18:08:57 +0100

    temp

--- Src/AdventOfCode2022/Days/Day16.cs
+++ Src/AdventOfCode2022/Days/Day16.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text.RegularExpressions;
 using AdventOfCode.Shared;
-using static AdventOfCode2022.Days.Day16;
 
 namespace AdventOfCode2022.Days
 {
@@ -25,8 +24,8 @@ namespace AdventOfCode2022.Days
         public static void StartA()
         {
             var lines = File
-                .ReadAllLines("Content\\Day16_Test.txt")
-                //.ReadAllLines("Content\\Day16.txt")
+                //.ReadAllLines("Content//Day16_Test.txt")
+                .ReadAllLines("Content//Day16.txt")
                 .Select(x =>
                 {
                     var match = Regex.Match(x, "Valve (.*?) has flow rate=(.*?); tunnel(s){0,1} lead(s){0,1} to valve(s){0,1} (.*)");
@@ -54,113 +53,215 @@ namespace AdventOfCode2022.Days
                 }
             });
 
-            var paths = new Dictionary<Valve, Dictionary<Valve, int>>();
-            foreach (var line in lines)
+            var start = lines.First(x => x.valve.Name == "AA").valve;
+
+            //Do a BFS
+            var queue = new Queue<(int time, Valve location, int score, HashSet<Valve> opened)>();
+            queue.Enqueue((1, start, 0, new HashSet<Valve>()));
+
+            var visited = new Dictionary<(int time, Valve location), int>();
+            var best = 0;
+            
+            while (queue.Count > 0)
             {
-                var result = ALen(line.valve);
-
-                paths.Add(line.valve, result);
-            }
-
-            var answer = 0;
-
-            Logger.Info($"Day 16A: {answer}");
-        }
-
-        //NOTE: But probably more like Dijkstra?
-        private static Dictionary<Valve, int> ALen(Valve start)
-        {
-            var openSet = new Queue<Valve>();
-
-            var gScore = new Dictionary<Valve, int>
-            {
-                [start] = 0
-            };
-
-            var closedSet = new HashSet<Valve>();
-
-            openSet.Enqueue(start);
-
-            while (openSet.Count > 0)
-            {
-                var valve = openSet.Dequeue();
-
-                foreach (var tunnel in valve.Tunnels)
+                var v = queue.Dequeue();
+                
+                if (visited.GetValueOrDefault((v.time, v.location), -1) >= v.score)
                 {
-                    if (closedSet.Contains(tunnel))
+                    continue;
+                }
+                
+                visited[(v.time, v.location)] = v.score;
+
+                if (v.time == 30)
+                {
+                    best = Math.Max(best, v.score);
+                    
+                    continue;
+                }
+                
+                //Open valve
+                if (v.location.FlowRate > 0 && !v.opened.Contains(v.location))
+                {
+                    var opened = new HashSet<Valve>(v.opened)
                     {
-                        continue;
+                        v.location
+                    };
+
+                    var score = v.score + opened.Sum(x => x.FlowRate);
+                    var state = (v.time + 1, v.location, score, opened);
+                    queue.Enqueue(state);
+                }
+                
+                //Go somewhere
+                {
+                    var score = v.score + v.opened.Sum(x => x.FlowRate);
+
+                    foreach (var tunnel in v.location.Tunnels)
+                    {
+                        var opened = new HashSet<Valve>(v.opened);
+                        var state = (v.time + 1, tunnel, score, opened);
+                        queue.Enqueue(state);
                     }
-
-                    closedSet.Add(tunnel);
-
-                    gScore[tunnel] = gScore[valve] + 1;
-
-                    openSet.Enqueue(tunnel);
                 }
             }
 
-            return gScore;
+            var answer = best;
+            
+            Logger.Info($"Day 16A: {answer}");
         }
-
+        
         public static void StartB()
         {
             var lines = File
-                .ReadAllLines("Content\\Day16_Test.txt")
-                //.ReadAllLines("Content\\Day16.txt")
+                    .ReadAllLines("Content//Day16_Test.txt")
+                    //.ReadAllLines("Content//Day16.txt")
+                    .Select(x =>
+                    {
+                        var match = Regex.Match(x, "Valve (.*?) has flow rate=(.*?); tunnel(s){0,1} lead(s){0,1} to valve(s){0,1} (.*)");
+
+                        return (
+                            valve: new Valve
+                            {
+                                Name = match.Groups[1].Value,
+                                FlowRate = int.Parse(match.Groups[2].Value),
+                                Tunnels = new List<Valve>()
+                            }, 
+                            tunnels: match.Groups[6].Value.Split(",").Select(y => y.Trim()).ToList()
+                        );
+                    })
+                    .ToList()
                 ;
 
-            var answer = 0;
+            var tunnelLookup = lines.ToDictionary(x => x.valve.Name);
 
-            Logger.Info($"Day 16B: {answer}");
-        }
-
-        private static List<Valve> AStar(Valve start, int highestScore)
-        {
-            var openSet = new PriorityQueue<(Valve valve, List<Valve> path, bool open, int minute), int>();
-            var parentMap = new Dictionary<Valve, Valve>();
-
-            var gScore = new Dictionary<Valve, int>
+            lines.ForEach(x =>
             {
-                [start] = 0
-            };
-
-            openSet.Enqueue((start, new List<Valve>(), false, 1), 0);
-
-            while (openSet.Count > 0)
-            {
-                var s = openSet.Dequeue();
-
-                if (s.minute >= 30)
+                foreach (var tunnel in tunnelLookup[x.valve.Name].tunnels)
                 {
-                    return s.path;
+                    x.valve.Tunnels.Add(tunnelLookup[tunnel].valve);
+                }
+            });
+
+            var start = lines.First(x => x.valve.Name == "AA").valve;
+            
+            //Do a BFS
+            var queue = new Queue<(int time, Valve location, Valve elephant, int score, HashSet<Valve> opened)>();
+            queue.Enqueue((1, start, start, 0, new HashSet<Valve>()));
+
+            var visited = new Dictionary<(int time, Valve location, Valve elephant), int>();
+            var best = 0;
+
+            var maxFlow = lines.Sum(x => x.valve.FlowRate);
+            
+            while (queue.Count > 0)
+            {
+                var v = queue.Dequeue();
+                
+                if (visited.GetValueOrDefault((v.time, v.location, v.elephant), -1) >= v.score)
+                {
+                    continue;
                 }
                 
-                var node = s.valve;
+                visited[(v.time, v.location, v.elephant)] = v.score;
 
-                var list = new List<Valve>(node.Tunnels);
-
-                if (s.open)
+                if (v.time == 26)
                 {
-
+                    best = Math.Max(best, v.score);
+                    
+                    continue;
                 }
+                
+                var currentFlow = v.opened.Sum(x => x.FlowRate);
 
-                foreach (var neighbor in list)
+                //We are done before time!
+                if (currentFlow >= maxFlow)
                 {
-                    var cost = gScore[node] + 1;
+                    var score = v.score + currentFlow;
+                    var time = v.time;
+                    
+                    while (time < 25)
+                    {
+                        time += 1;
+                        score += currentFlow;
+                    }
+                    
+                    var opened = new HashSet<Valve>(v.opened);
+                    var state = (v.time + 1, v.location, v.elephant, score, opened);
+                    queue.Enqueue(state);
+                    
+                    continue;
+                }
+                
+                //Open valve
+                if (v.location.FlowRate > 0 && !v.opened.Contains(v.location))
+                {
+                    var opened = new HashSet<Valve>(v.opened)
+                    {
+                        v.location
+                    };
 
-                    parentMap[neighbor] = node;
-                    gScore[neighbor] = cost;
+                    //Elephant opens valve
+                    if (v.elephant.FlowRate > 0 && !opened.Contains(v.elephant))
+                    {
+                        var openedWithElephant = new HashSet<Valve>(opened)
+                        {
+                            v.elephant
+                        };
+                        
+                        var score = v.score + openedWithElephant.Sum(x => x.FlowRate);
+                        var state = (v.time + 1, v.location, v.elephant, score, openedWithElephant);
+                        queue.Enqueue(state);
+                    }
 
-                    var fScore = highestScore - neighbor.FlowRate;
+                    //Elephant goes somewhere
+                    {
+                        var score = v.score + opened.Sum(x => x.FlowRate);
 
-                    var path = new List<Valve>(s.path) { node };
+                        foreach (var tunnel in v.elephant.Tunnels)
+                        {
+                            var state = (
+                                v.time + 1, tunnel, v.elephant, score, 
+                                new HashSet<Valve>(opened)
+                            );
+                            queue.Enqueue(state);
+                        }
+                    }
+                }
+                
+                //Go somewhere
+                foreach (var tunnel in v.location.Tunnels)
+                {
+                    //Elephant opens valve
+                    if (v.elephant.FlowRate > 0 && !v.opened.Contains(v.elephant))
+                    {
+                        var openedWithElephant = new HashSet<Valve>(v.opened)
+                        {
+                            v.elephant
+                        };
+                        
+                        var score = v.score + openedWithElephant.Sum(x => x.FlowRate);
+                        var state = (v.time + 1, tunnel, v.elephant, score, openedWithElephant);
+                        queue.Enqueue(state);
+                    }
 
-                    openSet.Enqueue((neighbor, path, false, s.minute + 1), cost + fScore);
+                    //Elephant goes somewhere
+                    {
+                        var score = v.score + v.opened.Sum(x => x.FlowRate);
+
+                        foreach (var tunnel2 in v.elephant.Tunnels)
+                        {
+                            var opened = new HashSet<Valve>(v.opened);
+                            var state = (v.time + 1, tunnel, tunnel2, score, opened);
+                            queue.Enqueue(state);
+                        }
+                    }   
                 }
             }
 
-            return null;
+            var answer = best;
+            
+            Logger.Info($"Day 16B: {answer}");
         }
     }
 }
