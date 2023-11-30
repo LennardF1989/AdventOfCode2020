74fbd8d6e249f8f234f89b36dbaa8200eb6d525d
Lennard Fonteijn <lennard@lennardf1989.com>
2023-11-30 23:49:36 +0100

    Added preparation for Day 1 of AoC2023

---
+++ Src/AdventOfCode2023/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AdventOfCode.Shared;
+using AdventOfCode2023.Days;
+
+namespace AdventOfCode2023
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger.ShowDebug = false;
+            
+            //if(false)
+            {
+                Leaderboard.Start();
+            }
+
+            Logger.ShowDebug = true;
+
+            Day01.StartA();
+            Day01.StartB();
+
+            Console.ReadKey();
+        }
+    }
+}
