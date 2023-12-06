4bdf8d1f8d00a901f933ed76b0ff8a028589befd
Lennard Fonteijn <lennard@lennardf1989.com>
2023-12-06 22:57:11 +0100

    Added Day 6 of AoC2023

--- Src/AdventOfCode2023/Days/Day06.cs
+++ Src/AdventOfCode2023/Days/Day06.cs
@@ -1,32 +1,78 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AdventOfCode.Shared;
+using AdventOfCode.Shared.Extensions;
 
 namespace AdventOfCode2023.Days
 {
     public static class Day06
     {
+        public record Input(long time, long distance);
+
         public static void StartA()
         {
             var lines = File
-                .ReadAllLines("Content\\Day06_Test.txt")
-                //.ReadAllLines("Content\\Day06.txt")
-                ;
+                //.ReadAllLines("Content\\Day06_Test.txt")
+                .ReadAllLines("Content\\Day06.txt")
+                .SelectList(x => x
+                    .Split(":", true)[1]
+                    .Split(" ", true, true)
+                    .SelectList(y => y.ToInteger())
+                );
 
-            var answer = 0;
+            var inputs = lines[0]
+                .Select((x, i) => new Input(x, lines[1][i]))
+                .ToList();
 
-            Logger.Info($"Day A: {answer}");
+            var answer = CalculateAnswer(inputs);
+
+            Logger.Info($"Day 6A: {answer}");
         }
 
         public static void StartB()
         {
             var lines = File
-                .ReadAllLines("Content\\Day06_Test.txt")
-                //.ReadAllLines("Content\\Day06.txt")
-                ;
+                //.ReadAllLines("Content\\Day06_Test.txt")
+                .ReadAllLines("Content\\Day06.txt")
+                .SelectList(x => x
+                    .Split(":", true)[1]
+                    .Replace(" ", string.Empty)
+                    .ToLong()
+                );
 
-            var answer = 0;
+            var inputs = new List<Input>
+            {
+                new(lines[0], lines[1])
+            };
 
-            Logger.Info($"Day B: {answer}");
+            var answer = CalculateAnswer(inputs);
+
+            Logger.Info($"Day 6B: {answer}");
+        }
+
+        private static long CalculateAnswer(List<Input> inputs)
+        {
+            var answer = 1;
+
+            foreach (var input in inputs)
+            {
+                var totalPossibilities = 0;
+
+                for (var startTime = 0; startTime <= input.time; startTime++)
+                {
+                    var distance = (input.time - startTime) * startTime;
+
+                    if (distance > input.distance)
+                    {
+                        totalPossibilities++;
+                    }
+                }
+
+                answer *= totalPossibilities;
+            }
+
+            return answer;
         }
     }
 }
--- Src/AdventOfCode2023/Program.cs
+++ Src/AdventOfCode2023/Program.cs
@@ -27,13 +27,16 @@ namespace AdventOfCode2023
                 Day04.StartB();
 
                 Day05.StartA();
-                Day05.StartB();
+                //Day05.StartB();
+
+                Day06.StartA();
+                Day06.StartB();
             }
 
             Logger.ShowDebug = true;
 
-            Day06.StartA();
-            Day06.StartB();
+            Day07.StartA();
+            Day07.StartB();
 
             Console.ReadKey();
         }
